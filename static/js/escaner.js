04aa906f5d757434a144224987ca4aef650be908
melanylibia <elibialany@gmail.com>
2024-05-16 18:13:04 -0400

    feat: adicion de frontend

--- static/js/escaner.js
+++ static/js/escaner.js
@@ -1,24 +1,24 @@
 function generateCookieValue() {
-    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    let cookieValue = '';
-    for (let i = 0; i < 32; i++) {
-      cookieValue += chars.charAt(Math.floor(Math.random() * chars.length));
-    }
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let cookieValue = '';
+  for (let i = 0; i < 32; i++) {
+    cookieValue += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return cookieValue;
+}
+function getCookieValue() {
+  const name = 'visitor_id';
+  const cookieValue = document.cookie.split('; ').find(row => row.startsWith(name + '='))?.split('=')[1];
+  if (cookieValue) {
     return cookieValue;
+  } else {
+    const newCookieValue = generateCookieValue();
+    document.cookie = `${name}=${newCookieValue}; path=/; max-age=31536000`; // Set cookie to expire in 1 year
+    return newCookieValue;
   }
-  function getCookieValue() {
-    const name = 'visitor_id';
-    const cookieValue = document.cookie.split('; ').find(row => row.startsWith(name + '='))?.split('=')[1];
-    if (cookieValue) {
-      return cookieValue;
-    } else {
-      const newCookieValue = generateCookieValue();
-      document.cookie = `${name}=${newCookieValue}; path=/; max-age=31536000`; // Set cookie to expire in 1 year
-      return newCookieValue;
-    }
-  }
-  const visitorId = getCookieValue();
-  console.log('Visitor ID:', visitorId);
+}
+const visitorId = getCookieValue();
+console.log('Visitor ID:', visitorId);
 
 function estoyvivo() {
   fetch('/estoyvivo', {
@@ -28,39 +28,55 @@ function estoyvivo() {
     },
     body: JSON.stringify({ visitorId })
   })
-  .then(response => {
-    if (response.ok) {
-      console.log('POST request successful');
-    } else {
-      console.error('POST request failed');
-    }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+    .then(response => {
+      if (response.ok) {
+        console.log('POST request successful');
+      } else {
+        console.error('POST request failed');
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
 
-setInterval(estoyvivo, 60000);
-estoyvivo();
+// setInterval(estoyvivo, 60000);
+// estoyvivo();
 
-function getNavegadorActual() {
-    //TODO: Lib agrega una URL con datos de verdad
-    fetch('/navegador_actual?navegador=Firefox&version=90')
+function getNavegadorActual(name, major) {
+  fetch(`/navegador_actual?navegador=${name}&version=${major}`)
     .then(response => {
       if (response.ok) {
         respuesta = response.text();
-        const navegadorActualSpan = document.getElementById('navegador_actual');
-        if (respuesta == "OK") {
-            navegadorActualSpan.textContent = "Si es actual";
-        } else if (respuesta == "NOK") {
-            navegadorActualSpan.textContent = "No es actual";
+        if (respuesta instanceof Promise) {
+          respuesta.then(data => {
+            const navegadorActualSpan = document.getElementById('navegador_actual');
+            navegadorActualSpan.textContent = name + " " + major + " ";
+            if (data == "OK") {
+              navegadorActualSpan.textContent =navegadorActualSpan.textContent + "Si es actual";
+            } else if (data == "NOK") {
+              navegadorActualSpan.textContent =navegadorActualSpan.textContent + "No es actual";
+            } else {
+              navegadorActualSpan.textContent =navegadorActualSpan.textContent + "No se pudo determinar";
+            }
+          }).catch(error => {
+            console.error('Error occurred:', error);
+          });
         } else {
-            navegadorActualSpan.textContent = "No se pudo determinar";
+          console.log('The variable is not a Promise.');
         }
+        // const navegadorActualSpan = document.getElementById('navegador_actual');
+        // if (respuesta == "OK") {
+        //     navegadorActualSpan.textContent = "Si es actual";
+        // } else if (respuesta == "NOK") {
+        //     navegadorActualSpan.textContent = "No es actual";
+        // } else {
+        //     navegadorActualSpan.textContent = "No se pudo determinar";
+        // }
       } else {
         throw new Error('GET request failed');
       }
     });
 }
 
-getNavegadorActual();
+// getNavegadorActual();
